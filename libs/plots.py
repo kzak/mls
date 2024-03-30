800cff478b431e1f8a1cdf34ec1603dc8a1f6f2f
zak <15907+kzak@users.noreply.github.com>
2024-03-30 14:15:00 +0900

    Mod: BayesianLinearRegressor

--- libs/plots.py
+++ libs/plots.py
@@ -59,9 +59,7 @@ def plotly_true_and_prediction_surface(true_fn, prediction_fn, X, y, **kwargs):
     # Build visualizing objects
     # ------------------------------
     true_surface = plotly_build_surface(true_fn, r=r, colorscale="Reds", opacity=0.2)
-    prediction_surface = plotly_build_surface(
-        prediction_fn, r=r, colorscale="Blues", opacity=0.5
-    )
+    prediction_surface = plotly_build_surface(prediction_fn, r=r, colorscale="Blues", opacity=0.5)
     scatter_3d = plotly_build_scatter3d(X, y, color="red")
 
     fig = go.Figure(data=[prediction_surface, true_surface, scatter_3d])
@@ -72,9 +70,7 @@ def plotly_true_and_prediction_surface(true_fn, prediction_fn, X, y, **kwargs):
     fig.update_layout(
         autosize=False,
         margin=dict(l=0, r=0, t=10, b=10),
-        scene=dict(
-            xaxis=dict(title="x1"), yaxis=dict(title="x2"), zaxis=dict(title="y")
-        ),
+        scene=dict(xaxis=dict(title="x1"), yaxis=dict(title="x2"), zaxis=dict(title="y")),
         showlegend=False,
     )
 
--- libs/syn_datasets.py
+++ libs/syn_datasets.py
@@ -2,9 +2,25 @@ import numpy as np
 
 
 # ------------------------------
+# 1D datasets
+# ------------------------------
+class SinDataset1D:
+    def __init__(self, N=30, noise=0.2):
+        self.X = np.random.uniform(low=-np.pi, high=np.pi, size=(N,))
+        self.noise = noise
+        self.y = self.true_fn(self.X) + noise * np.random.normal(scale=1.0, size=(N,))
+
+    def true_fn(self, X):
+        return np.sin(X)
+
+    def load_data(self):
+        return self.X, self.y
+
+
+# ------------------------------
 # 2D datasets
 # ------------------------------
-class SinExpDataset:
+class SinExpDataset2D:
     def __init__(self, N=30, noise=0.2):
         self.X = np.random.uniform(low=-np.pi, high=np.pi, size=(N, 2))
         self.noise = noise
