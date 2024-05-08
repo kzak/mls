71d1c772ba024de109afd5c26275360d9e05992f
zak <15907+kzak@users.noreply.github.com>
2024-05-08 14:43:41 +0900

    modified:   models/rff_regressor.py 	new file:   notebooks/rff_regression.ipynb 	modified:   poetry.lock 	modified:   pyproject.toml

--- models/rff_regressor.py
+++ models/rff_regressor.py
@@ -2,20 +2,25 @@ import numpy as np
 
 
 class RffRegressor:
-    def __init__(self, lmd=1.0e-3, n_rff=30):
+    def __init__(self, lmd=1.0e-3, n_rff=100, intercept=True):
         self.lmd = lmd
         self.n_rff = n_rff  # The number of random Fourier features
-        self.alpha = None  # (n_rff, )
+        self.intercept = intercept
+        self.weights = None  # (n_rff, )
 
     def _rff(self, X):
-        # (N, D) -> (N, n_rff)
-        Wx = X @ self.Omg  # (N, D) @ (D, n_rff) -> (N, n_rff)
-        b = self.b  # (1, n_rff) to be broadcasted to (N, n_rff)
-        Wx_b = Wx + b  # (N, n_rff)
-        X_rff = np.sqrt(2 / self.n_rff) * np.cos(Wx_b)
-        return X_rff
+        # (N, D) -> (N, n_rff) or (N, n_rff + 1)
 
-    # Optimize in weight space view
+        Omg_X = X @ self.Omg  # (N, D) @ (D, n_rff) -> (N, n_rff)
+        Omg_X_b = Omg_X + self.b  # (N, n_rff)
+
+        # Random Fourier features
+        Phi_X = np.sqrt(2 / self.n_rff) * np.cos(Omg_X_b)
+        if self.intercept:
+            Phi_X = np.hstack([np.ones(len(X)).reshape(-1, 1), Phi_X])
+
+        return Phi_X
+
     def fit(self, X, y):
         self.X = X
         self.y = y
@@ -25,15 +30,16 @@ class RffRegressor:
         self.b = np.random.uniform(0, 2 * np.pi, size=(1, self.n_rff))  # (1, n_rff)
 
         _y = y.reshape(-1, 1) if y.ndim == 1 else y  # (N, 1)
-        _X = self._rff(self.X)  # (N, n_rff)
+        Phi_X = self._rff(self.X)  # (N, n_rff)
 
-        A = (_X.T @ _X) + self.lmd * np.eye(self.n_rff)  # (n_rff, n_rff)
-        b = _X.T @ _y  # (D, 1)
-        self.alpha = np.linalg.solve(A, b)  # (n_rff, )
+        # Solve linear system: A x = b
+        A = Phi_X.T @ Phi_X + self.lmd * np.eye(Phi_X.shape[1])  # (n_rff, n_rff)
+        b = Phi_X.T @ _y  # (D, 1)
+        self.weights = np.linalg.solve(A, b)  # (n_rff, )
 
         return self
 
     def predict(self, X):
-        _X = self._rff(X)  # (M, n_rff)
-        y_hat = (_X @ self.alpha).ravel()  # (M, )
+        Phi_X = self._rff(X)  # (M, n_rff)
+        y_hat = (Phi_X @ self.weights).ravel()  # (M, )
         return y_hat
